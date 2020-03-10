516d70603f373b25173c02426f2c11d6ea1c468f
bwakedavis <bwakedavis@gmail.com>
2020-03-10 10:13:39 +0300

    build a production deploy

---
+++ build/precache-manifest.2d3bbbdf96c60085ebd941b74e23fb66.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0c6291c1ea3469691463192f98010fd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d0ad1f680b3d1f446c1e",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "cdda00271fa581330107",
+    "url": "/static/css/main.6a934dfc.chunk.css"
+  },
+  {
+    "revision": "d0ad1f680b3d1f446c1e",
+    "url": "/static/js/2.30cd0ce2.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.30cd0ce2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cdda00271fa581330107",
+    "url": "/static/js/main.aaba0130.chunk.js"
+  },
+  {
+    "revision": "e06c96b9be6927f5c0c3",
+    "url": "/static/js/runtime-main.10bc7ff2.js"
+  },
+  {
+    "revision": "f3d994ab179426aaf1ce019bc5187f90",
+    "url": "/static/media/giphy.f3d994ab.gif"
+  }
+]);
