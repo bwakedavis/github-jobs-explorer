86c66d38957339d385b88847a75fa6a6d63ec801
bwakedavis <bwakedavis@gmail.com>
2020-03-10 09:46:15 +0300

    successfully fetched data with axios and display top tracks,artist name and album name

---
+++ src/tracks/Track.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import {Link} from 'react-router-dom';
+function Track(props) {
+    const {track} = props
+    return (
+        <div className="col-md-6">
+            <div className="card mb-4 shadow-sm">
+                <div className="card-body">
+                    <h5>{track.artist_name}</h5>
+                    <p className="card-text">
+    <strong>Track : {track.track_name}</strong>
+    <strong>Track : {track.album_name}</strong>
+                    </p>
+
+                    <Link to={`lyrcs/track/${track.track_id}`} className="btn btn-dark btn-block">View lyrics</Link>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Track
--- src/tracks/Tracks.js
+++ src/tracks/Tracks.js
@@ -1,18 +1,29 @@
 import React, { Component } from 'react'
 import { Consumer } from '../Context'
 import Spinner from '../components/layout/Spinner'
+import Track from './Track'
 
 class Tracks extends Component {
     render() {
         return (
             <Consumer>
                 {value=>{
-                    const {track_list} = value
+                    const {track_list,heading} = value
                     console.log(value)
                     if(track_list ===undefined|| track_list.length ===0){
                         return <Spinner/>
                     }else{
-                        return <h1>Tracks loaded</h1>
+                        return (
+                            <React.Fragment>
+                                <h3 className="text-center mb-4">{heading}</h3>
+                            <div className='row'>
+                            {track_list.map((item)=> (
+                                <Track key= {item.track.track_id} track ={item.track} />
+                            ))}
+                            </div>
+
+                            </React.Fragment>
+                        )
                     }
                 }}
                
